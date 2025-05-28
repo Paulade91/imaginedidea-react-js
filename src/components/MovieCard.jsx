9edb819e07ebc74a9eb672c44be303fb4cd9b75d
Paulade91 <paulannadebby@yahoo.com>
2025-05-28 22:39:44 +0000

    added the trailer btn

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,31 @@
 function MovieCard({ title, description, image, rating }) {
-    const ratingClass = "text-bg-danger";
+  const ratingClass =
+    rating >= 8
+      ? "text-bg-success"
+      : rating >= 5
+      ? "text-bg-warning"
+      : "text-bg-danger";
+
+  const clickTrailer = () => {
+    alert("🎬 Trailer coming soon!");
+  };
+
   return (
     <>
-      <div className="card" style={{ width: '18rem', margin: '20px'}}>
+      <div className="card" style={{ margin: "20px" }}>
         <img src={image} className="card-img-top" alt={title} />
         <div className="card-body">
-          <h5 className="card-title">{ title }</h5>
+          <h5 className="card-title">{title}</h5>
           <p className="card-text"> {description} </p>
-          <p className="card-text"> 
-                <span className={`badge rounded-pill ${ratingClass}`}> {rating}/10</span>
+          <p className="card-text">
+            <button onClick={clickTrailer} className="btn btn-primary">
+              Watch Trailer
+            </button>{" "}
+            <br />
+            <span className={`badge rounded-pill ${ratingClass}`}>
+              {" "}
+              {rating}/10
+            </span>
           </p>
         </div>
       </div>
